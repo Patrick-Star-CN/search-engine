1fea27e8c55f38bd91f63c74001c75407b44b731
Patrick-Star-CN <1592810043@qq.com>
2022-05-23 19:52:39 +0800

    feat: accomplish build in linux env

--- app/utils/wordCutter/wordCutter.go
+++ app/utils/wordCutter/wordCutter.go
@@ -1,9 +1,21 @@
 package wordCutter
 
-import "github.com/yanyiwu/gojieba"
+import (
+	"github.com/yanyiwu/gojieba"
+	"os"
+	"path"
+	"path/filepath"
+)
 
 func WordCut(source string) []string {
-	x := gojieba.NewJieba()
+	dictDir := path.Join(filepath.Dir(os.Args[0]), "dict")
+	jiebaPath := path.Join(dictDir, "jieba.dict.utf8")
+	hmmPath := path.Join(dictDir, "hmm_model.utf8")
+	userPath := path.Join(dictDir, "user.dict.utf8")
+	idfPath := path.Join(dictDir, "idf.utf8")
+	stopPath := path.Join(dictDir, "stop_words.utf8")
+	x := gojieba.NewJieba(jiebaPath, hmmPath, userPath, idfPath, stopPath)
+
 	defer x.Free()
 	var wordsSlice []string
 	wordMap := make(map[string]int)
